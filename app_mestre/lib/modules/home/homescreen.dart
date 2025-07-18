e8a307bda26c5477550101895927f48dc5ce5362
Victor Hugo <victor.030farias@gmail.com>
2025-07-18 02:35:45 -0300

    Inicia e testa o projeto usando o widget do quadrado rosa

---
+++ app_mestre/lib/modules/home/homescreen.dart
@@ -0,0 +1,11 @@
+import 'package:app_mestre/widgets/pink_square_widget.dart';
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: PinkSquareWidget());
+  }
+}
